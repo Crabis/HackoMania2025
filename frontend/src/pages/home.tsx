b1494ec5a0a8915c1bfa86fbb761aafd8f0643bd
blueetfm <tan.yx2004@gmail.com>
2025-02-15 17:11:27 +0800

    constants

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -69,7 +69,7 @@ export default function Page() {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: "center" }}>
-              Title
+              Recovering Warriors
             </Typography>
             <IconButton color="inherit">
               <AccountCircleIcon />
